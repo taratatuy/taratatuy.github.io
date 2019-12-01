deead3fd1ce698f30f6e318e8cea78fc3c89a6b1
Alexandr Timofeev <taratatuy@yandex.ru>
2019-12-01 20:18:34 +0300

    Code refactoring

--- DailyRadio/src/assets/js/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const HTTP = axios.create({
-    baseURL: `https://dailyradio-server.herokuapp.com/`,
-    // headers: {
-    //     Authorization: 'Bearer {token}'
-    // }
-})
