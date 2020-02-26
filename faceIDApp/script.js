bb9c2287b4742af6f180780977170f7a3b16ba6b
Alexandr Timofeev <taratatuy@yandex.ru>
2020-02-26 21:30:22 +0300

    face id first try

--- faceIDApp/script.js
+++ faceIDApp/script.js
@@ -1,30 +1,41 @@
 const video = document.getElementById('video');
 
-/* Setting up the constraint */
-var facingMode = 'environment'; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
-var constraints = {
-  audio: false,
-  video: {
-    // facingMode: facingMode
-  }
-};
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
 
-/* Stream it to video element */
-console.log(navigator);
-navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-  video.srcObject = stream;
+function startVideo() {
+  let constraints = {
+    audio: false,
+    video: {
+      height: 720,
+      width: 720,
+      facingMode: 'environment' // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    video.srcObject = stream;
+  });
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100);
 });
-
-// function startVideo() {
-//   navigator.getUserMedia(
-//     { video: { width: 720, height: 720 } },
-//     stream => {
-//       video.srcObject = stream;
-//     },
-//     err => {
-//       console.log(err);
-//     }
-//   );
-// }
-
-// startVideo();
